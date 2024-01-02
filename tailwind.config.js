ef89f60e3da1e2a96f6357cfbecf55618aa7feef
Cody Williams <cody.williams@untappd.com>
2024-01-01 20:22:06 -0500

    tailwind component config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,9 @@
 import formsPlugin from '@tailwindcss/forms';
 import typographyPlugin from '@tailwindcss/typography';
+import * as aspectRatio from '@tailwindcss/aspect-ratio';
 
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ['./app/**/*.{js,ts,jsx,tsx}'],
-  plugins: [formsPlugin, typographyPlugin],
+  plugins: [formsPlugin, typographyPlugin, aspectRatio],
 };
